ddb47a8298dda016c3a8dd16007ad8c9c0ea5afe
Ambar Noatay <68185571+ambarnoatay@users.noreply.github.com>
2024-06-18 18:54:54 -0500

    Update Cache.h

--- SRC/Cache.h
+++ SRC/Cache.h
@@ -1,28 +1,67 @@
 #ifndef CACHE_H
 #define CACHE_H
-
+#include <vector>
+#include<math.h>
 #include <unordered_map>
 #include <cstdint>  // Include this for uint64_t
 #include "types.h"
+class Cache_line {
+public:
+    bool valid;
+    bool dirty;
+    Addr tag;
+    uint64_t last_access_time;
+    int frequency;
+    int core_id;
+    Cache_line():valid(false), dirty(false), tag(0), last_access_time(0), frequency(0), core_id(0) {}
+
+
+};
+class Cache_set {
+public:
+    Cache_line* line;
+    Cache_set(int ways):line(new Cache_line[ways]) {}
+    ~Cache_set() {
+        delete[] line;
+    }
+
+
+
+};
+
+
 
 class Cache {
 public:
-    Cache(int size, int associativity);
-
+    Cache(int size, int associativity, int lineSize);
+    
+    void printStats(); 
+    void printCache(); 
+    int cache_lookup(Addr address,int write); 
+    void cache_insert(Addr address);
+    void cache_remove_lru(Addr address);
+    void updateLRU();
     // Function to read data from the cache
-    bool readData(uint64_t address, int& data) const;
+    bool readData(Addr address, int& data) const;
 
     // Function to write data to the cache
-    void writeData(uint64_t address, int data);
+    void writeData(Addr address, int data);
 
 private:
     int size;            // Cache size in bytes
     int associativity;   // Cache associativity
-    int blockSize;       // Cache block size
-    int numSets;         // Number of sets in the cache
-    std::unordered_map<uint64_t, int> cacheData;  // Cache data storage
+    int lineSize;       // Cache block size
+    int linesPerSet;         // Number of sets in the cache
+    int lookups;
+    int tag_bits;
+    int index_bits;
+    int offset_bits;
+    int hits;
+
+    std::vector<Cache_set*> sets;
     
     // Other necessary members and functions for cache implementation
 };
+
 
 #endif  // CACHE_H
